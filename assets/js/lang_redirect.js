40ce1dac1014f86d22626dd3d089dd1c54ff32ea
Benau <Benau@users.noreply.github.com>
2022-01-25 08:26:20 +0000

    jekyll build from Action 20d87381320f5e40397927cc7878a8127bed4002

---
+++ assets/js/lang_redirect.js
@@ -0,0 +1,269 @@
+var translations = {"Contents":{"bg":"Съдържание","fr":"Contenus","ca":"Continguts","pl":"Spis treści","da":"Indhold","nl":"Inhoud","pt_BR":"Conteúdo","es":"Contenidos","pt":"Conteúdos","de":"Inhalt","zh_TW":"目錄"},"Menu":{"zh_CN":"菜单","bg":"Меню","ca":"Menú","es":"Menú","de":"Menü","zh_TW":"目錄"},"Discover":{"zh_CN":"探索","bg":"Разгледайте","fr":"Découvrir","ca":"Descobreix","pl":"Odkryj","da":"Opdag","nl":"Ontdekken","pt_BR":"O Jogo","es":"Descubrir","pt":"Descobrir","de":"Entdecke","zh_TW":"探索"},"Download":{"zh_CN":"下载","bg":"Сваляне","fr":"Télécharger","ca":"Descarregar","pl":"Pobierz","nl":"Downloaden","pt_BR":"Baixar","es":"Descargar","pt":"Instalar","zh_TW":"下載"},"FAQ":{"zh_CN":"常问问题","bg":"ЧЗВ","ca":"PMF","nl":"Veelgestelde vragen","pt_BR":"Perguntas Frequentes","zh_TW":"常見問題解答"},"Get Involved":{"zh_CN":"参与","bg":"Включете се","fr":"Contribuer","ca":"Participa","pl":"Zaangażuj się","da":"Hjælp med","nl":"Help mee","pt_BR":"Contribua","es":"Participa","pt":"Involve-te","de":"Mach mit","zh_TW":"參與"},"Blog":{"zh_CN":"博客","bg":"Блог","zh_TW":"部落格"},"Donate":{"zh_CN":"捐款","bg":"Даряване","fr":"Soutenir","ca":"Dona","pl":"Darowizny","da":"Donér","nl":"Doneren","pt_BR":"Doações","es":"Dona","pt":"Doar","de":"Unterstütze uns","zh_TW":"捐款"},"Tools":{"bg":"Инструменти","fr":"Outils","ca":"Eines","pl":"Opcje","da":"Værktøjer","nl":"Hulpmiddelen","pt_BR":"Ferramentas","es":"Herramientas","pt":"Ferramentas","de":"Werkzeuge","zh_TW":"工具"},"Language":{"bg":"Език","fr":"Langue","ca":"Idioma","pl":"Język","da":"Sprog","nl":"Taal","pt_BR":"Idioma","es":"Idioma","pt":"Idioma","de":"Sprache","zh_TW":"語言"},"Management":{"bg":"Управление","fr":"Gestion","ca":"Gestió","pl":"Zarządzanie","da":"Behandling","nl":"Beheer","pt_BR":"Gerenciamento","es":"Gestión","pt":"Manutenção","de":"Verwaltung","zh_TW":"管理"},"New page":{"bg":"Нова страница","fr":"Nouvelle page","ca":"Nova pàgina","pl":"Nowa strona","da":"Ny side","nl":"Nieuwe pagina","pt_BR":"Nova página","es":"Nueva página","pt":"Página nova","de":"Neue Seite","zh_TW":"建立新頁面"},"Edit page":{"bg":"Редактиране на страницата","fr":"Modifier la page","ca":"Edita la pàgina","pl":"Edytuj stronę","da":"Redigér side","nl":"Pagina bewerken","pt_BR":"Editar página","es":"Edita la página","pt":"Editar página","de":"Seite bearbeiten","zh_TW":"編輯頁面"},"Page history":{"bg":"История на страницата","fr":"Historique","ca":"Historial de la pàgina","pl":"Historia zmian na stronie","da":"Sidehistorik","nl":"Paginageschiedenis","pt_BR":"Histórico","es":"Historial de la página","pt":"Histórico da página","de":"Seitenverlauf","zh_TW":"頁面歷史"},"Visit the main page":{"zh_CN":"访问主页","bg":"Към главната страница","fr":"Aller à la page d'accueil","ca":"Visita la pàgina principal","pl":"Przejdź na stronę główną","da":"Besøg hjemmesiden","nl":"Ga naar de hoofdpagina","pt_BR":"Voltar à página inicial","es":"Visita la página principal","pt":"Visitar a página inicial","de":"Besuche die Hauptseite","zh_TW":"造訪首頁"},"Community":{"zh_CN":"社区","bg":"Общност","fr":"Communauté","ca":"Comunitat","pl":"Społeczność","da":"Fællesskab","nl":"Gemeenschap","pt_BR":"Comunidade","es":"Comunidad","pt":"Comunidade","zh_TW":"社群"},"Forum":{"zh_CN":"论坛","bg":"Форум","ca":"Fòrum","pt_BR":"Fórum","es":"Foro","pt":"Fórum","zh_TW":"論壇"},"Twitter":{"zh_CN":"推特"},"Addons":{"zh_CN":"附加组件","bg":"Добавки","fr":"Extensions","ca":"Complements","pl":"Dodatki","da":"Tilføjelser","nl":"Uitbreidingen","pt_BR":"Complementos","es":"Complementos","pt":"Add-Ons","de":"Add-ons","zh_TW":"附加元件"},"Telegram":{"pt":"Telegrama"},"Media":{"zh_CN":"媒体","bg":"Медии","fr":"Presse","ca":"Mitjans","pl":"Multimedia","da":"Medier","pt_BR":"Vídeos e Imagens","es":"Medios","de":"Medien","zh_TW":"媒體"},"YouTube":{"pt":"Youtube"},"Gallery":{"zh_CN":"画廊","bg":"Галерия","fr":"Gallerie","ca":"Galeria","pl":"Galeria","da":"Galleri","nl":"Galerij","pt_BR":"Galeria","es":"Galería","pt":"Galeria","de":"Gallerie","zh_TW":"畫廊"},"Posters":{"zh_CN":"海报","bg":"Плакати","ca":"Pòsters","pl":"Plakaty","da":"Plakater","es":"Pósters","de":"Poster","zh_TW":"海報"},"Development":{"zh_CN":"开发","bg":"Разработка","fr":"Développement","ca":"Desenvolupament","pl":"Rozwój","da":"Udvikling","nl":"Ontwikkeling","pt_BR":"Desenvolvimento","es":"Desarrollo","pt":"Desenvolvimento","de":"Entwicklung ","zh_TW":"開發"},"Modding":{"zh_CN":"改装","bg":"Модификации","fr":"Participer","pl":"Modowanie","nl":"Mods","pt_BR":"Modificação","de":"Modifizierungen","zh_TW":"改裝"},"GitHub":{},"Documentation":{"zh_CN":"文件","bg":"Документация","ca":"Documentació","pl":"Dokumentacja","da":"Dokumentation","nl":"Documentatie","pt_BR":"Documentação","es":"Documentación","pt":"Documentação","de":"Dokumentation","zh_TW":"文件"},"About us":{"zh_CN":"关于我们","bg":"За нас","fr":"À propos","ca":"Quant a nosaltres","pl":"O nas","da":"Om os","nl":"Over ons","pt_BR":"Quem somos","es":"Acerca de nosotros","pt":"Sobre nós","de":"Über uns","zh_TW":"關於我們"},"About the team":{"zh_CN":"关于团队","bg":"За екипа","fr":"À propos de l'équipe","ca":"Quant a l'equip","pl":"Zespół gry SuperTuxKart","da":"Om teamet","nl":"Over het team","pt_BR":"Nossa equipe","es":"Acerca del equipo","pt":"Sobre a equipa","de":"Über das Team","zh_TW":"關於團隊"},"About SuperTuxKart":{"zh_CN":"关于 SuperTuxKart","bg":"За Супер Тъкс Карт","fr":"À propos de SuperTuxKart","ca":"Quant al SuperTuxKart","pl":"O grze SuperTuxKart","da":"Om SuperTuxKart","nl":"Over SuperTuxKart","pt_BR":"Sobre o SuperTuxKart","es":"Acerca del SuperTuxKart","pt":"Sobre SuperTuxKart","de":"Über SuperTuxKar","zh_TW":"關於 SuperTuxKart"},"Projects using SuperTuxKart":{"zh_CN":"使用 SuperTuxkart 的项目","bg":"Проекти, използващи Супер Тъкс Карт","fr":"Projets qui utilisent SuperTuxKart","ca":"Projectes que fan servir el SuperTuxKart","pl":"Projekty używające gry SuperTuxKart","da":"Projekter der bruger SuperTuxKart","nl":"Projecten die gebruikmaken van SuperTuxKart","pt_BR":"Projetos que usam o SuperTuxKart","es":"Proyectes que utilizan el SuperTuxKart","pt":"Projetos que using SuperTuxKart","de":"Projekte mit SuperTuxKart","zh_TW":"使用 SuperTuxKart 的專案"},"Info":{"bg":"Информация","fr":"Informations","ca":"Informació","pl":"Informacje","da":"Information","nl":"Informatie","pt_BR":"Informações","es":"Información","pt":"Informação","zh_TW":"資訊"},"Return to Get Involved":{"bg":"Обратно към „Включете се“","fr":"Retourner à Contribuer","ca":"Torna a Participa","pl":"Powrót do strony Zaangażuj się","da":"Gå tilbage til Hjælp med","nl":"Terug naar ‘Bijdragen’","pt_BR":"Voltar para Contribua","es":"Volver a Participa","de":"Zurück zu \"Mach Mit\""},"Making Library Nodes":{"bg":"Създаване на обекти от вида „Library Node“","fr":"Créer des objets de librairie","ca":"Fent \"library nodes\"","pl":"Tworzenie Obiektów 3D","da":"Fremstille biblioteksknudepunkter","nl":"Verzamelingskoppelingen maken","pt_BR":"Nós de bibliotecas","es":"Haciendo \"library nodes\"","pt":"Fazer Library Nodes","de":"Erstellen von Bibliotheks-Knoten","zh_TW":"製作函式庫節點"},"Making Karts":{"bg":"Създаване на колички","fr":"Créer des karts","ca":"Fent karts","pl":"Tworzenie Gokartów","da":"Fremstille Karts","nl":"Karts maken","pt_BR":"Karts","es":"Haciendo karts","pt":"Fazer Karts","de":"Erstellen von Karts","zh_TW":"製作卡丁車"},"Making Tracks":{"bg":"Създаване на писти","fr":"Créer des pistes","ca":"Fent circuits","pl":"Tworzenie Tras","da":"Fremstille baner","nl":"Parcours maken","pt_BR":"Pistas","es":"Haciendo circuitos","pt":"Fazer Pistas","de":"Erstellen von Strecken","zh_TW":"製作賽道"},"Code":{"bg":"Код","ca":"Codi","pl":"Kod","da":"Kode","pt_BR":"Código","es":"Código","pt":"Código","zh_TW":"程式碼"},"Prerequisites":{"bg":"Предварителни условия","fr":"Prérequis","ca":"Prerequisits","pl":"Wymagania","da":"Forudsætninger","nl":"Vereisten","pt_BR":"Pré-requisitos","es":"Prerrequisitos","de":"Vorraussetzungen"},"You should read the following articles prior to following this tutorial:":{"bg":"Трябва да прочетете следните статии, преди да започнете с инструкциите:","fr":"Vous devriez lire les articles suivants avant de suivre ce tutoriel :","ca":"Hauríeu de llegir els articles següents abans de seguir aquest tutorial:","pl":"Powinieneś przeczytać następujące artykuły zanim zaczniesz śledzić ten poradnik:","da":"Du bør læse disse artikler, før du følger denne denne vejledning:","nl":"Lees voorafgaand aan deze handleiding de volgende artikelen:","pt_BR":"Você deve ler os seguintes artigos antes de seguir este tutorial:","es":"Deberías leer los siguientes artículos antes de seguir este tutorial:","de":"Du solltest die folgenden Artikel gelesen haben, bevor du dem Tutorial folgst."},"Show":{"bg":"Показване","fr":"Montrer","ca":"Mostra","pl":"Pokaż","da":"Vis","nl":"Tonen","pt_BR":"Exibir","es":"Mostrar","pt":"Mostrar","de":"anzeigen","zh_TW":"顯示"},"Hide":{"bg":"Скриване","fr":"Cacher","ca":"Amaga","pl":"Ukryj","da":"Skjul","nl":"Verbergen","pt_BR":"Ocultar","es":"Ocultar","pt":"Esconder","de":"verbergen","zh_TW":"隱藏"}};
+// ES5 support from https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/startsWith#polyfill
+if (!String.prototype.startsWith)
+{
+    Object.defineProperty(String.prototype, 'startsWith',
+    {
+        value: function(search, rawPos)
+        {
+            var pos = rawPos > 0 ? rawPos|0 : 0;
+            return this.substring(pos, pos + search.length) === search;
+        }
+    });
+}
+
+// var translations will be added by _plugins/translate.rb
+var supported_languages = ["bg","ca","da","de","en","es","fr","nl","pl","pt","pt_BR","zh_CN","zh_TW"];
+var fallback_languages = ["fr","zh_TW"];
+var page_translations = {"Voice_Acting":["nl","pl","es","bg","pt_BR","ca","fr","de","pt"],"Texture_Guidelines":["nl","pl","es","bg","pt_BR","ca","fr"],"Source_control":["nl","pl","es","bg","pt_BR","ca","fr","zh_TW","de"],"Posters":["nl","pl","es","bg","pt_BR","ca","fr","zh_TW","de","pt"],"Media_Repo":["nl","pl","es","bg","pt_BR","ca","fr","de"],"Main_Page":["zh_CN","bg","fr","ca","pl","da","nl","pt_BR","es","pt","de","zh_TW"],"FAQ":["nl","pl","es","bg","pt_BR","ca","fr","de","pt"],"Download":["zh_CN","bg","fr","ca","pl","da","nl","pt_BR","es","pt","de","zh_TW"],"Donation_Policy":["nl","pl","es","bg","pt_BR","ca","fr","da","de","pt"],"Donate":["nl","pl","es","bg","pt_BR","ca","fr","zh_TW","de","pt"],"Discover":["zh_CN","bg","fr","ca","pl","da","nl","pt_BR","es","pt","de","zh_TW"],"Community":["zh_CN","bg","fr","ca","pl","da","nl","pt_BR","es","pt","de","zh_TW"],"Communication":["nl","pl","es","bg","pt_BR","ca","fr","da","de","pt"],"3D_Model_Guidelines":["es","bg","pt_BR","ca","fr"]};
+var language_names = {"bg":"български","ca":"Català","da":"Dansk","de":"Deutsch","en":"English","es":"Español","fr":"Français","fr-ca":"Français (Canada)","nl":"Nederlands","pl":"Polski","pt":"Português","pt_BR":"Português Brasileiro","zh_CN":"中文（简体）","zh_TW":"中文（繁體）","zh_TW-hk":"中文（香港）",};
+
+function getSuitableLang(lang, locale)
+{
+    var real_locale = locale;
+    // Special handling for possible tranditional chinese locales to avoid
+    // displaying simplified chinese for those users
+    if (lang == 'zh')
+    {
+        if (locale.search('hk') != -1 || locale.search('mo') != -1)
+        {
+            real_locale = 'hk';
+            locale = 'TW';
+        }
+        else if (locale.search('tw') != -1 || locale.search('hant') != -1)
+        {
+            real_locale = 'tw';
+            locale = 'TW';
+        }
+        else
+        {
+            real_locale = 'cn';
+            locale = 'CN';
+        }
+    }
+    sessionStorage.setItem('preferred_locale', real_locale);
+
+    locale = locale.toUpperCase();
+    for (var i = 0; i < supported_languages.length; i++)
+    {
+        var lang_locale = lang + '_' + locale;
+        if (supported_languages[i] == lang_locale)
+            return supported_languages[i];
+    }
+    for (var i = 0; i < supported_languages.length; i++)
+    {
+        var lang_only = supported_languages[i].split('_')[0]
+        if (lang_only == lang)
+            return supported_languages[i];
+    }
+    return null;
+}
+
+var site_url = 'https://benau.github.io' + '/stk-website' + '/';
+var page = window.location.href.substring(site_url.length).split("/").pop();
+var page_only = page.split('#')[0];
+
+function getURLLanguage(url)
+{
+    if (!url.startsWith(site_url))
+        return '';
+    var sl = url.slice(site_url.length).split('/')[0];
+    if (supported_languages.indexOf(sl) != -1)
+        return sl;
+    return '';
+}
+
+function changeLanguage(lang)
+{
+    var lang_locale = lang.split('-');
+    lang = lang_locale[0];
+    var locale = '';
+    if (lang_locale.length == 2)
+        locale = lang_locale[1];
+    sessionStorage.setItem('preferred_locale', locale);
+    try
+    {
+        sessionStorage.setItem('overridden_lang', lang);
+        if (page_translations.hasOwnProperty(page_only) &&
+            page_translations[page_only].indexOf(lang) != -1)
+        {
+            window.location.replace(site_url + lang + '/' + page_only);
+        }
+        else
+        {
+            window.location.replace(site_url + page_only);
+        }
+    } catch (error) {}
+}
+
+var lis = [];
+for (cur_lang in language_names)
+{
+    var new_node = document.getElementById('language-selector-en').cloneNode(true);
+    new_node.id = 'language-selector-' + cur_lang;
+    var ahref = new_node.getElementsByTagName('A')[0];
+    if (cur_lang != 'en' && fallback_languages.indexOf(cur_lang) == -1 &&
+        page_translations.hasOwnProperty(page_only) &&
+        page_translations[page_only].indexOf(cur_lang) != -1)
+    {
+        ahref.href = site_url + cur_lang + '/' + page_only;
+    }
+    else
+    {
+        ahref.href = 'javascript:changeLanguage("' + cur_lang + '");';
+    }
+    ahref.textContent = language_names[cur_lang];
+    lis.push(new_node);
+}
+
+document.getElementById('language-selector').innerHTML = '';
+for (var i = 0; i < lis.length; i++)
+{
+    document.getElementById('language-selector').appendChild(lis[i]);
+}
+
+var preferred_lang = document.documentElement.lang.replace("-", "_");
+var preferred_locale = '';
+var redirect = false;
+try
+{
+    if (!sessionStorage.getItem('first_visit'))
+    {
+        sessionStorage.setItem('first_visit', true);
+        redirect = document.referrer.length == 0 ||
+            document.referrer == site_url;
+    }
+
+    // Prevent error when cookies disabled
+    if (!sessionStorage.getItem('preferred_lang'))
+    {
+        sessionStorage.setItem('preferred_lang', 'en');
+
+        var user_language = (((navigator.userLanguage || navigator.language).replace('-', '_')).toLowerCase()).split('_');
+        var lang = user_language[0];
+        var locale = '';
+        if (user_language.length > 1)
+            locale = user_language[1];
+        var suitable_lang = getSuitableLang(lang, locale)
+        if (suitable_lang != null)
+            sessionStorage.setItem('preferred_lang', suitable_lang);
+    }
+    if (sessionStorage.getItem('preferred_locale'))
+        preferred_locale = sessionStorage.getItem('preferred_locale');
+
+    if (sessionStorage.getItem('overridden_lang'))
+    {
+        preferred_lang = sessionStorage.getItem('overridden_lang');
+        sessionStorage.setItem('preferred_lang', preferred_lang);
+        sessionStorage.removeItem('overridden_lang');
+    }
+    else
+    {
+        // Override preferred language if language tag is supplied in url
+        // or the referrer
+        var site_url_lang = getURLLanguage(window.location.href);
+        if (site_url_lang == '')
+            site_url_lang = getURLLanguage(document.referrer);
+        if (site_url_lang != '')
+            sessionStorage.setItem('preferred_lang', site_url_lang);
+        preferred_lang = sessionStorage.getItem('preferred_lang');
+    }
+} catch (error) {}
+
+var doc_lang = document.documentElement.lang.replace("-", "_");
+if (doc_lang == 'en' && doc_lang != preferred_lang)
+{
+    // Only redirect if page translation exists and first time visit the page
+    // without referrer (so search engine clicking is not affected),
+    // otherwise translate the layout texts and links
+    var success = false;
+    if (redirect && page_translations.hasOwnProperty(page))
+    {
+        var arr = page_translations[page];
+        for (var i = 0; i < arr.length; i++)
+        {
+            if (preferred_lang == arr[i])
+            {
+                window.location.replace(site_url + preferred_lang + '/' + page);
+                success = true;
+                break;
+            }
+        }
+    }
+    if (!success && doc_lang == 'en')
+    {
+        // Special locale handling in the end
+        doc_lang = preferred_lang;
+        var logo = document.getElementsByClassName('logo noselect')[0];
+        var logo_text = logo.getAttribute('title');
+        if (translations.hasOwnProperty(logo_text) &&
+            translations[logo_text].hasOwnProperty(preferred_lang))
+            logo.setAttribute('title', translations[logo_text][preferred_lang]);
+
+        // Table of contents markdown toggle [Show] / [Hide] handling
+        var markdown_toggle = document.getElementById('markdown-toggle');
+        if (markdown_toggle != null)
+        {
+            var show_text = markdown_toggle.getAttribute('data-show-text');
+            if (translations.hasOwnProperty(show_text) &&
+                translations[show_text].hasOwnProperty(preferred_lang))
+                markdown_toggle.setAttribute('data-show-text', translations[show_text][preferred_lang]);
+            var hide_text = markdown_toggle.getAttribute('data-hide-text');
+            if (translations.hasOwnProperty(hide_text) &&
+                translations[hide_text].hasOwnProperty(preferred_lang))
+                markdown_toggle.setAttribute('data-hide-text', translations[hide_text][preferred_lang]);
+        }
+
+        var elements = document.getElementsByClassName("translate");
+        for (var i = 0; i < elements.length; i++)
+        {
+            var element_text = elements[i].textContent;
+            if (translations.hasOwnProperty(element_text) &&
+                translations[element_text].hasOwnProperty(preferred_lang))
+                elements[i].textContent = translations[element_text][preferred_lang];
+        }
+        var links = document.links;
+        for (var i = 0; i < links.length; i++)
+        {
+            if (!links[i].href.startsWith(site_url))
+                continue;
+            var link_page = links[i].href.split("/").pop();
+            if (!page_translations.hasOwnProperty(link_page))
+                continue;
+            var arr = page_translations[link_page];
+            for (var j = 0; j < arr.length; j++)
+            {
+                if (preferred_lang == arr[j])
+                {
+                    links[i].href = site_url + preferred_lang + '/' + link_page;
+                    break;
+                }
+            }
+        }
+    }
+}
+
+var locale_key = preferred_lang + '-' + preferred_locale;
+var locale_dict = {"zh_TW-hk":{"部落格":"網誌"},"fr-ca":{"football":"soccer","Football":"Soccer"}};
+if (doc_lang == preferred_lang && locale_dict.hasOwnProperty(locale_key))
+{
+    var walker = document.createTreeWalker(
+        document.body,
+        NodeFilter.SHOW_TEXT,
+        function acceptNode(node)
+        {
+            return NodeFilter.FILTER_ACCEPT;
+        },
+        false);
+    var n;
+    while (n = walker.nextNode())
+    {
+        if (n.textContent.trim().length == 0)
+            continue;
+        var locale_data = locale_dict[locale_key];
+        for (key in locale_data)
+        {
+            n.textContent = n.textContent.replace(key, locale_data[key]);
+        }
+    }
+}
